207d300c2207686408ee17d571d1791e9f0c7a05
anton-b117 <anton@maroonspear.com>
2015-05-09 23:01:36 -0400

    Coded boot.js, index.js, index.jade, and NoBackMon.js.

---
+++ public/boot.js
@@ -0,0 +1,17 @@
+require.config({
+	paths: {
+		jQuery: '/js/libs/jquery',
+		Underscore:  '/js/libs/underscore',
+		Backbone:  '/js/libs/backbone',
+		text:  '/js/libs/text',
+		templates:  '../templates'
+	}, 
+	shim: {
+		'Backbone': ['Underscore', 'jQuery'],
+		'NoBackMon': ['Backbone']
+	}
+});
+
+require(['NoBackMon'], function(NoBackMon) {
+	NoBackMon.initialize();
+});
---
+++ public/js/NoBackMon.js
@@ -0,0 +1,9 @@
+define(['views/index'], funcion(indexView) {
+	var initialize = function() {
+		indexView.render();
+	}
+
+	return  {
+		initialize: initialize
+	};
+});
